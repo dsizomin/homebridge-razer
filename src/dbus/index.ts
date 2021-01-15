5427751705e17e36ae6c7f4c4427cc324a16b21e
Denys Sizomin <denys@miro.com>
2021-01-16 00:36:13 +0100

    Added hue&saturation controls.

--- src/dbus/index.ts
+++ src/dbus/index.ts
@@ -1,4 +1,5 @@
 import { getBus } from 'dbus';
+import convert from 'color-convert';
 
 import type { DBusConnection, DBusInterface } from 'dbus';
 
@@ -68,15 +69,6 @@ export async function getAllDevices(): Promise<Device[]> {
 
 export async function setBrightness(device: Device, value: number): Promise<void> {
   const brightnessDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.brightness', device.serial);
-  const chromaDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.chroma', device.serial);
-
-  await new Promise((res, rej) => {
-    if (value) {
-      chromaDbusInterface.setStatic(137, 35, 26, (err) => err ? rej(err) : res());
-    } else {
-      chromaDbusInterface.setNone((err) => err ? rej(err) : res());
-    }
-  });
 
   await new Promise((res, rej) => {
     brightnessDbusInterface.setBrightness(value, (err) => err ? rej(err) : res());
@@ -89,6 +81,62 @@ export async function getBrightness(device: Device): Promise<number> {
   return new Promise((res, rej) => {
     dbusInterface.getBrightness((err, value) => err ? rej(err) : res(Number(value)));
   });
+}
+
+export async function setHue(device: Device, hue: number): Promise<void> {
+  const chromaDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.chroma', device.serial);
+
+  const [red, green, blue] = await new Promise((res, rej) => {
+    chromaDbusInterface.getEffectColors((err, value) => err ? rej(err) : res(value));
+  });
+
+  const [h, s, v] = convert.rgb.hsv([red, green, blue]);
+
+  const [newRed, newGreen, newBlue] = convert.hsv.rgb([hue, s, v]);
+
+  return new Promise((res, rej) => {
+    chromaDbusInterface.setStatic(newRed, newBlue, newGreen, (err) => err ? rej(err) : res());
+  });
+}
+
+export async function getHue(device: Device): Promise<number> {
+  const chromaDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.chroma', device.serial);
+
+  const [red, green, blue] = await new Promise((res, rej) => {
+    chromaDbusInterface.getEffectColors((err, value) => err ? rej(err) : res(value));
+  });
+
+  const [h] = convert.rgb.hsv([red, green, blue]);
+
+  return Number(h);
+}
+
+export async function setSaturation(device: Device, saturation: number): Promise<void> {
+  const chromaDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.chroma', device.serial);
+
+  const [red, green, blue] = await new Promise((res, rej) => {
+    chromaDbusInterface.getEffectColors((err, value) => err ? rej(err) : res(value));
+  });
+
+  const [h, s, v] = convert.rgb.hsv([red, green, blue]);
+
+  const [newRed, newGreen, newBlue] = convert.hsv.rgb([h, saturation, v]);
+
+  return new Promise((res, rej) => {
+    chromaDbusInterface.setStatic(newRed, newBlue, newGreen, (err) => err ? rej(err) : res());
+  });
+}
+
+export async function getSaturation(device: Device): Promise<number> {
+  const chromaDbusInterface: DBusInterface = await getDBusInterface('razer.device.lighting.chroma', device.serial);
+
+  const [red, green, blue] = await new Promise((res, rej) => {
+    chromaDbusInterface.getEffectColors((err, value) => err ? rej(err) : res(value));
+  });
+
+  const [h, s] = convert.rgb.hsv([red, green, blue]);
+
+  return Number(s);
 }
 
 export async function setOn(device: Device, value: boolean): Promise<void> {
